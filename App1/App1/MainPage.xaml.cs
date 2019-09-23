1f7992bd587faabf8ac94ba4c0d566f4e7596438
Kristian Behrendt <kristianbg20@mail.com>
2019-09-23 10:13:01 +0200

    first commit

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            button.Clicked += Button_Clicked;
+        }
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            string name = entry.Text;
+            await DisplayAlert("Hi there", $"Hello {name}", "OK");
+
+        }
+    }
+}
